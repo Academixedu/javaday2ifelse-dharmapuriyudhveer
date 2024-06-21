eba468f356228a36787829d1a65521b14c20b53c
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-06-21 11:24:20 +0530

    YeaH

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -8,7 +8,28 @@ public class StudentGradeEvaluation {
 
         // Prompt the user to enter their name
         System.out.print("Enter your name: ");
+        String name=scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age=scanner.nextInt();
+
+        System.out.println("Enter your exam score: ");
+        int score=scanner.nextInt();
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Score : " + score);
         
+        if(score>=90)
+            System.out.println("Grade: A");
+        else if(score >= 80 && score < 90)
+            System.out.println("Grade: B");
+            else if(score >= 70 && score < 80)
+            System.out.println("Grade: C");
+        else if(score >= 60 && score < 70)
+            System.out.println("Grade: D");
+        else
+            System.out.println("Grade: F");
 
         // Prompt the user to enter their age
         
